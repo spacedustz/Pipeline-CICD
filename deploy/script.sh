89f73407333bd3047556e836320d92d81350e0b6
KunWoo Shin <cyberspecterr@gmail.com>
2023-05-27 00:47:58 +0900

    Test Script

--- deploy/script.sh
+++ deploy/script.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+echo hello
+
 # 가동중인 Spring Boot 컨테이너 중지 & 삭제
-sudo docker ps -a -q --filter "name=cosmic" | grep -q . && docker stop cosmic && docker rm cosmic | true
+docker ps -a -q --filter "name=cosmic" | grep -q . && docker stop cosmic && docker rm cosmic | true
 
 # 기존 Spring Boot Image 삭제
-sudo docker rmi 43.201.243.115:5000/cosmic:1.0
+docker rmi 43.201.243.115:5000/cosmic:1.0
 
 # Docker Private Container Registry에 이미지 Pull
-sudo docker pull 43.201.243.115:5000/cosmic:1.0
+docker pull 43.201.243.115:5000/cosmic:1.0
 
 # Docker run
-sudo docker run -d --privileged --name cosmic -p 8080:8080 -v /root/logs:/logs 43.201.243.115:5000/cosmic:1.0
+docker run -d --privileged --name cosmic -p 8080:8080 -v /root/logs:/logs 43.201.243.115:5000/cosmic:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제
-sudo docker rmi -f ${docker images -f "dangling=true" -q} || true
+docker rmi -f ${docker images -f "dangling=true" -q} || true
