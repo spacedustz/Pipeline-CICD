e2bc18d0f520c94848978045cb9aea987cca08da
spacedustz <tensorflow555@gmail.com>
2023-05-27 01:30:55 +0900

    Done

--- deploy/script.sh
+++ deploy/script.sh
@@ -3,6 +3,9 @@
 # 가동중인 Spring Boot 컨테이너 중지 & 삭제
 docker ps -a -q --filter "name=cosmic" | grep -q . && docker stop cosmic && docker rm cosmic | true
 
+# 컨테이너 종료 & 삭제될 때까지 대기
+sleep 3s
+
 # 기존 Spring Boot Image 삭제
 docker rmi 43.201.243.115:5000/cosmic:1.0
 
