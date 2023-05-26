179fdf09ab4a5ec25d57ea610afa2468896ff681
Kunwoo Shin <recyclefunction@gmail.com>
2023-05-27 01:08:59 +0900

    Almost Done

--- deploy/script.sh
+++ deploy/script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo hello
-
 # 가동중인 Spring Boot 컨테이너 중지 & 삭제
 docker ps -a -q --filter "name=cosmic" | grep -q . && docker stop cosmic && docker rm cosmic | true
 
@@ -15,4 +13,5 @@ docker pull 43.201.243.115:5000/cosmic:1.0
 docker run -d --privileged --name cosmic -p 8080:8080 -v /root/logs:/logs 43.201.243.115:5000/cosmic:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제
-docker rmi -f ${docker images -f "dangling=true" -q} || true
+dangling_images=$(docker images -f "dangling=true" -q)
+docker rmi -f ${dangling_images} || true
