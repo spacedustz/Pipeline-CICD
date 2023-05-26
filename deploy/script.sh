15d6c7cd31a47411e6bed5d8ce9b09c238f5ba4c
owen <wavefunctionc@gmail.com>
2023-05-26 15:23:47 +0900

    Add Shell Script

--- deploy/script.sh
+++ deploy/script.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+chmod 500 /var/jenkins_home/workspace/Test-CICD/deploy/script.sh
 
+# 가동중인 Spring Boot 컨테이너 중지 & 삭제
+sudo docker ps -a -q --filter "name=cosmic" | grep -q . && docker stop cosmic && docker rm cosmic | true
+
+# 기존 Spring Boot Image 삭제
+sudo docker rmi backend/cosmic:1.0
+
+# Docker Private Container Registry에 이미지 Pull
+sudo docker pull backend/cosmic:1.0
+
+# Docker run
+sudo docker run -d --name cosmic -p 8080:8080 -v /root/logs:/logs backend/cosmic:1.0
+
+# 사용하지 않는 불필요한 이미지 삭제
+sudo docker rmi -f ${docker images -f "dangling=true" -q} || true
