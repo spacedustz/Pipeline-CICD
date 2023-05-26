4698543fab08bd96e4db23016281c5cacb854328
KunWoo Shin <cyberspecterr@gmail.com>
2023-05-27 01:42:33 +0900

    Done

--- deploy/script.sh
+++ deploy/script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # 가동중인 Spring Boot 컨테이너 중지 & 삭제
-docker ps -a -q --filter "name=cosmic" | grep -q . && docker stop cosmic && docker rm cosmic | true
+#docker ps -a -q --filter "name=cosmic" | grep -q . && docker stop cosmic && docker rm cosmic | true
+containers=$(docker ps -a --filter "ancestor=43.201.243.115:5000/cosmic:1.0" -q)
+if [[ -n "$containers" ]]; then
+  docker stop $containers
+  docker rm $containers
+fi
 
 # 컨테이너 종료 & 삭제될 때까지 대기
 sleep 3s
